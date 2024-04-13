ba00c959b259cab05311faa82cad9e66672ea5b0
Joshua Portero <77929472+joshuaportero@users.noreply.github.com>
2024-04-13 10:46:46 -0400

    Small class refactor

--- src/main/java/dev/portero/xenon/configuration/ConfigurationManager.java
+++ src/main/java/dev/portero/xenon/configuration/ConfigurationManager.java
@@ -1,14 +1,14 @@
 package dev.portero.xenon.configuration;
 
+import dev.portero.xenon.bukkit.position.Position;
 import dev.portero.xenon.configuration.composer.DurationComposer;
 import dev.portero.xenon.configuration.composer.LanguageComposer;
 import dev.portero.xenon.configuration.composer.MaterialComposer;
 import dev.portero.xenon.configuration.composer.PositionComposer;
 import dev.portero.xenon.configuration.composer.SetComposer;
+import dev.portero.xenon.feature.language.Language;
 import dev.portero.xenon.injector.annotations.Inject;
 import dev.portero.xenon.injector.annotations.component.Service;
-import dev.portero.xenon.feature.language.Language;
-import dev.portero.xenon.bukkit.position.Position;
 import dev.portero.xenon.multification.cdn.MultificationNoticeCdnComposer;
 import dev.portero.xenon.multification.notice.Notice;
 import net.dzikoysk.cdn.Cdn;
