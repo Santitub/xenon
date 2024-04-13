8137b33d192808e0ccd7e39efc1f1b06958d7675
Joshua Portero <77929472+joshuaportero@users.noreply.github.com>
2024-04-13 10:54:22 -0400

    Refactored NoticeBroadcastHandler to support generic types in NoticeBroadcast

--- src/main/java/dev/portero/xenon/bridge/litecommand/handler/NoticeBroadcastHandler.java
+++ src/main/java/dev/portero/xenon/bridge/litecommand/handler/NoticeBroadcastHandler.java
@@ -2,17 +2,19 @@ package dev.portero.xenon.bridge.litecommand.handler;
 
 import dev.portero.xenon.injector.annotations.lite.LiteHandler;
 import dev.portero.xenon.multification.notice.NoticeBroadcast;
+import dev.portero.xenon.translation.Translation;
+import dev.portero.xenon.viewer.Viewer;
 import dev.rollczi.litecommands.handler.result.ResultHandler;
 import dev.rollczi.litecommands.handler.result.ResultHandlerChain;
 import dev.rollczi.litecommands.invocation.Invocation;
 import org.bukkit.command.CommandSender;
 
 @LiteHandler(NoticeBroadcast.class)
-public class NoticeBroadcastHandler implements ResultHandler<CommandSender, NoticeBroadcast> {
+public class NoticeBroadcastHandler<T extends NoticeBroadcast<Viewer, Translation, T>> implements ResultHandler<CommandSender, NoticeBroadcast<Viewer, Translation, T>> {
+
 
     @Override
-    public void handle(Invocation<CommandSender> invocation, NoticeBroadcast result, ResultHandlerChain<CommandSender> chain) {
+    public void handle(Invocation<CommandSender> invocation, NoticeBroadcast<Viewer, Translation, T> result, ResultHandlerChain<CommandSender> chain) {
         result.send();
     }
-
 }
