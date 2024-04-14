c77093b3a59cd805eab97c888e6924786b0a06be
Joshua Portero <77929472+joshuaportero@users.noreply.github.com>
2024-04-14 12:51:18 -0400

    Moved essential commands to new essentials package

--- src/main/java/dev/portero/xenon/feature/essentials/fly/FlyCommand.java
+++ src/main/java/dev/portero/xenon/feature/essentials/fly/FlyCommand.java
@@ -1,4 +1,4 @@
-package dev.portero.xenon.feature.fly;
+package dev.portero.xenon.feature.essentials.fly;
 
 import dev.portero.xenon.injector.annotations.Inject;
 import dev.portero.xenon.notice.NoticeService;
