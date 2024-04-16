9a4a5e880351914b97ffbdb60957e655c5131292
Joshua Portero <77929472+joshuaportero@users.noreply.github.com>
2024-04-16 15:24:08 -0400

    Fix compiling error in HealCommand

--- src/main/java/dev/portero/xenon/feature/command/heal/HealCommand.java
+++ src/main/java/dev/portero/xenon/feature/command/heal/HealCommand.java
@@ -2,6 +2,7 @@ package dev.portero.xenon.feature.command.heal;
 
 import dev.portero.xenon.injector.annotations.Inject;
 import dev.portero.xenon.notice.NoticeService;
+import dev.rollczi.litecommands.annotations.argument.Arg;
 import dev.rollczi.litecommands.annotations.command.Command;
 import dev.rollczi.litecommands.annotations.context.Context;
 import dev.rollczi.litecommands.annotations.execute.Execute;
@@ -22,24 +23,12 @@ public class HealCommand {
         sender.setHealth(20);
         sender.setFoodLevel(20);
         sender.setSaturation(20);
-
-        this.noticeService.create()
-            .notice(translation -> translation.player().healPlayer())
-            .placeholder("{PLAYER}", sender.getName())
-            .player(sender.getUniqueId())
-            .send();
     }
 
     @Execute
-    public void execute(@Context Player sender, Player target) {
+    public void execute(@Context Player sender, @Arg Player target) {
         target.setHealth(20);
         target.setFoodLevel(20);
         target.setSaturation(20);
-
-        this.noticeService.create()
-            .notice(translation -> translation.player().healPlayer())
-            .placeholder("{PLAYER}", target.getName())
-            .player(target.getUniqueId())
-            .send();
     }
 }
